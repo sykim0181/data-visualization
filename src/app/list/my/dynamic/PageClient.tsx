38a5aa1462b0a0793b09e2370d17d814fac5d586
sykim0181 <soyeon364@naver.com>
2025-11-30 16:13:49 +0900

    chore: 행마다 다른 높이 주는 방식 변경

--- src/app/list/my/dynamic/PageClient.tsx
+++ src/app/list/my/dynamic/PageClient.tsx
@@ -3,6 +3,8 @@
 import MyDynamicVirtualList from "@/components/MyDynamicVirtualList";
 import { SalesRecord } from "@/lib/sales/types";
 
+const colors = ["#00A0FA", "#FF1C30", "#2BBFA7", "#FF00F7", "#FF9800"];
+
 const PageClient = ({ items }: { items: SalesRecord[] }) => {
   return (
     <MyDynamicVirtualList
@@ -19,9 +21,17 @@ const PageClient = ({ items }: { items: SalesRecord[] }) => {
             {item.salesChannel} | Units: {item.unitsSold.toLocaleString()} |
             Profit: {item.totalProfit.toLocaleString()}
           </div>
-          {/* 여기 일부러 텍스트 길이에 따라 높이 달라지게 */}
-          <p style={{ marginTop: item.country.length > 10 ? 4 : 10 * item.country.length }}>
-            주문일 {item.orderDate} | 수익 {item.totalProfit.toLocaleString()}{" "}
+          {/* {item.country.length > 10 && ( */}
+          <div
+            style={{
+              width: 20 * item.country.length,
+              height: 20 * item.country.length,
+              backgroundColor: colors[index % colors.length],
+            }}
+          />
+          {/* )} */}
+          <p>
+            주문일 {item.orderDate} | 수익 {item.totalProfit.toLocaleString()}
           </p>
         </div>
       )}
